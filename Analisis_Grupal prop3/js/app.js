915310f6c573b2576b37ddc1bdc8ad0da9c49055
Marlon Orellana <marlonorellana2005@gmail.com>
2022-06-26 19:12:47 -0500

    Update app.js Marlon@260622
    Actualizacion Domingo 26

--- Analisis_Grupal prop3/js/app.js
+++ Analisis_Grupal prop3/js/app.js
@@ -94,11 +94,12 @@ const valueCard4 = card4
     .append('div')
     .attr('class', 'chartLayer')
 
+// Espacio de Grafico de Tendencia -----------------------------------------------------------------
 const titleChart = chartLayer
     .append('div')
     .append('h3')
     .attr('class', 'titleChart')
-    .text('Analisis de tendencia')
+          .text('Analisis de tendencia')
 
 const chart = chartLayer
     .append('svg')
@@ -107,6 +108,9 @@ const chart = chartLayer
 const drawChart = chart
     .append('g')
     .attr('transform', `translate(${margins.left},${margins.top})`)
+
+ 
+//-------------------------------------------------------------------------------------------------
 
 const filter = layout
     .append('div')
@@ -328,6 +332,137 @@ const getVarLastYear = (country, category) => {
     }
 
 }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Dimensiones
+const anchoTotal = +chart.style("width").slice(0, -2)
+const altoTotal = (anchoTotal * 9) / 16
+
+//Graph print
+const ancho = anchoTotal - margins.left - margins.right
+const alto = altoTotal - margins.top - margins.bottom
+    
+    // Elementos gráficos (layers)
+    const svg = chart
+      .append("svg")
+      .attr("width", anchoTotal)
+      .attr("height", altoTotal)
+      .attr("class", "chart")
+      
+    drawChart
+      .append("rect")
+      .attr("height", alto)
+      .attr("width", ancho)
+      .attr("fill", "blue")
+    
+ 
+const draw = async ( ) => {     
+
+ //   data = await d3.csv('Dataset_inflation.csv', d3.autoType)
+
+    selectCountry
+    .selectAll("option")
+    .data(Object.keys(data[0]).slice(1))
+    .enter()
+    .append("option")
+    .attr("value", (d) => d)
+    .text((d) => d)
+
+  // Accessor
+  const xAccessor = (d) => d.Country
+
+  // Escaladores
+  const y = d3.scaleLinear().range([alto, 0])
+  const color = d3
+    .scaleOrdinal()
+    .domain(Object.keys(data[0]).slice(1))
+    .range(d3.schemeTableau10)
+
+  // console.log(data)
+  // console.log(d3.map(data, xAccessor))
+
+  const x = d3.scaleBand().range([0, ancho]).paddingOuter(0.2).paddingInner(0.1)
+
+//   const titulo = g
+//     .append("text")
+//     .attr("x", ancho / 2)
+//     .attr("y", -15)
+//     .classed("titulo", true)
+
+ const etiquetas = drawChart.append("g")
+
+  const xAxisGroup = drawChart
+    .append("g")
+    .attr("transform", `translate(0, ${alto})`)
+    .classed("axis", true)
+  const yAxisGroup = drawChart.append("g").classed("axis", true)
+
+  const render = (variable) => {
+    // Accesores
+    const yAccessor = (d) => d[variable]
+    data.sort((a, b) => yAccessor(b) - yAccessor(a))
+
+    // Escaladores
+    y.domain([0, d3.max(data, yAccessor)])
+    x.domain(d3.map(data, xAccessor))
+
+    // Rectángulos (Elementos)
+    const rect = drawChart.selectAll("rect").data(data, xAccessor)
+    rect
+      .enter()
+      .append("rect")
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", (d) => y(0))
+      .attr("width", x.bandwidth())
+      .attr("height", 0)
+      .attr("fill", "green")
+      .merge(rect)
+      .transition()
+      .duration(2500)
+      // .ease(d3.easeBounce)
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", (d) => y(yAccessor(d)))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => alto - y(yAccessor(d)))
+      .attr("fill", (d) =>
+        xAccessor(d) == "Satélite" ? "#f00" : color(variable)
+      )
+
+    const et = etiquetas.selectAll("text").data(data)
+    et.enter()
+      .append("text")
+      .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
+      .attr("y", (d) => y(0))
+      .merge(et)
+      .transition()
+      .duration(2500)
+      .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
+      .attr("y", (d) => y(yAccessor(d)))
+      .text(yAccessor)
+
+    // Títulos
+    titulo.text(`${variable} de las Tiendas`)
+
+    // Ejes
+    const xAxis = d3.axisBottom(x)
+    const yAxis = d3.axisLeft(y).ticks(8)
+    xAxisGroup.transition().duration(2500).call(xAxis)
+    yAxisGroup.transition().duration(2500).call(yAxis)
+  }
+
+  // Eventos
+  selectCountry.on("change", (e) => {
+    e.preventDefault()
+    // console.log(e.target.value, metrica.node().value)
+    render(e.target.value)
+  })
+  render()
+}
+
+draw()
+
+//----------------------------------------------------------------------------------------------------------------------------------
 
 checksCategory
     .selectAll('input')
