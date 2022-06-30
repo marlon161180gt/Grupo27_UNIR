a4113c84bedfdff238edc27befc208cdf76d9ec9
sespeje <107823069+sespeje@users.noreply.github.com>
2022-06-29 23:33:00 -0500

    Update app.js

--- Analisis_Grupal prop3/js/app.js
+++ Analisis_Grupal prop3/js/app.js
@@ -10,27 +10,27 @@ const margins = {
     left: 30,
 }
 
-// Layers
+// Capas - Elementos HTML
 
-// Title of analysis
+// Titulo del analisis
 const title = layout
     .append('div')
     .attr('class', 'title')
     .append('h2')
     .text('Analisis Inflacionario Global')
 
-// Layer for cards and chart
+// Espacio para las tarjetas y la grafica
     const chartGroup = layout
     .append('div')
     .attr('class', 'chartGroup')
 
-// Layer for cards
+// Espacio de las tarjetas
 const cardGroup = chartGroup
     .append('div')
     .attr('class', 'cardGroup')
     .attr('style', 'margin-bottom: 1%')
 
-// Cards
+// Tarjetas (titulo y valor)
 const card1 = cardGroup
     .append('div')
     .attr('class', 'card')
@@ -72,8 +72,7 @@ const titleCard3 = card3
 const valueCard3 = card3
     .append('div')
     .append('p')
-    .attr('class', 'dataCard')
-    // .text('10.58%')    
+    .attr('class', 'dataCard')  
 
 const card4 = cardGroup
     .append('div')
@@ -89,49 +88,55 @@ const valueCard4 = card4
     .append('p')
     .attr('class', 'dataCard')
 
-// Layer of chart
+// Espacio de la grafica
     const chartLayer = chartGroup
     .append('div')
     .attr('class', 'chartLayer')
 
-// Espacio de Grafico de Tendencia -----------------------------------------------------------------
+// Titulo de la grafica
 const titleChart = chartLayer
     .append('div')
     .append('h3')
     .attr('class', 'titleChart')
           .text('Analisis de tendencia')
 
+// Area de la grafica
 const chart = chartLayer
     .append('svg')
     .attr('class', 'chart')
 
+// Inicio de la grafica
 const drawChart = chart
     .append('g')
     .attr('class', 'drawChart')
     .attr('transform', `translate(${margins.left},${margins.top})`)
 
+// Espacio para los elementos rect
 const rectElements = drawChart
     .append('g')
     .attr('class', 'rectElements')
 
+// Espacio para las etiquetas de datos
 const labelsLayer = drawChart
     .append('g')
     .attr('class', 'labels')
 
+// Espacio para el eje x
 const xAxisLayer = drawChart
     .append('g')
     .attr('class', 'xAxis')
 
+// Espacio para el eje y
 const yAxisLayer = drawChart
     .append('g')
     .attr('class', 'yAxis')
     
-//-------------------------------------------------------------------------------------------------
-
+// Espacio para los filtros
 const filter = layout
     .append('div')
     .attr('class', 'filter')
 
+// Filtro de categoria
 const filterCategory = filter
     .append('div')
     .attr('class', 'filterCategory')
@@ -145,6 +150,7 @@ const checksCategory = filterCategory
     .append('div')
     .attr('class', 'checksCategory')
 
+// Filtro de País
 const filterCountry = filter
     .append('div')
     .attr('class', 'filterCountry')
@@ -161,6 +167,7 @@ const selectCountry = filterCountry
     .attr('class', 'form-select')
     .attr('style', 'margin-top: 5%')
 
+// Slider año (Filtro por año)
 const filterYear = filter
     .append('div')
     .attr('class', 'filterYear')
@@ -174,14 +181,17 @@ const rangeYear = filterYear
     .append('div')
     .attr('class', 'rangeYear')
 
+// Espacio para el diseño de los input y el slider
 const wrapper = rangeYear
     .append('div')
     .attr('class', 'wrapper')
 
+// Espacio para los input
 const price_input = wrapper
     .append('div')
     .attr('class', 'price-input')
 
+// Contenedor para valor minimo
 const fieldMin = price_input
     .append('div')
     .attr('class', 'field')
@@ -190,6 +200,7 @@ fieldMin
     .append('span')
     .text('Min')
 
+// Input para obtener el valor minimo
 fieldMin
     .append('input')
     .attr('type', 'number')
@@ -200,6 +211,7 @@ const separator = price_input
     .attr('class', 'separator')
     .text('-')
 
+// Contenedor para el valor maximo
 const fieldMax = price_input
     .append('div')
     .attr('class', 'field')
@@ -208,59 +220,64 @@ fieldMax
     .append('span')
     .text('Max')
 
+// Input para obtener el valor maximo
 fieldMax
     .append('input')
     .attr('type', 'number')
     .attr('class', 'input-max')
 
+// Contenedor del slider
 const slider = wrapper
     .append('div')
     .attr('class', 'slider')
     .append('div')
     .attr('class', 'progress')
 
-    
+// Contenedor para rangos
 const rangeInput = wrapper
     .append('div')
     .attr('class', 'range-input')
 
+// Elemento de rango minimo
 rangeInput
     .append('input')
     .attr('type', 'range')
     .attr('class', 'range-min')
 
-
+// Elemento de rango máximo
 rangeInput
     .append('input')
     .attr('type', 'range')
     .attr('class', 'range-max')
 
-// Load Data
+// Función para la carga de datos y creación de grafica
 const load = async () => {
     data = await d3.csv('New_DataInflation (2).csv', d3.autoType)
 
+    // Constantes para obtener un arreglo de elementos únicos 
     const distinctCountry = [... new Set(data.map((d) => d.Country))]
     const distinctCategory = [... new Set(data.map((d) => d.Category))]
     const distinctYears = [... new Set(data.map((d) => d.Year))]
 
+    // Constantes para obtener valor mínimo y máximo del arreglo de años (distinctYears)
     const minYear = Math.min(...distinctYears)
     const maxYear = Math.max(...distinctYears)
 
-    // Escalators
+    // Escaladores 
     const y = d3
         .scaleLinear()
         .range([drawChart.style('height').slice(0,-2), 0])
 
     const x = d3
         .scaleBand()
-        .range([0, drawChart.style('width').slice(0,-2)])
-        // .range([0, chart.node().getBoundingClientRect().width])
-        // .range([0, '100vw'])
+        // .range([0, drawChart.style('width').slice(0,-2)])
         .paddingOuter(0.2)
         .paddingInner(0.1)
 
-    // Axis
+    // Función para obtener rango de las 'x' dinamico
+    setDynamicXRange()
 
+    // Ejes de la grafica
     const xAxisGroup = xAxisLayer
         .append('g')
         .attr('transform', `translate(0, ${drawChart.style('height').slice(0,-2)})`)
@@ -270,41 +287,57 @@ const load = async () => {
         .append('g')
         .classed('axis', true)
 
+    // Función para dibujar la grafica parametros(país y categoría)
     const drawChartRect = (country, category) => {
 
-
+        // Validamos si nuestros parametros vienen vacíos
         if (country !== undefined && category !== undefined){
 
+            // Obtenemos los valores de los años minimo y maximo de nuestro slider
             const minVal = rangeInput.select('input.range-min').node().value,
             maxVal = rangeInput.select('input.range-max').node().value
 
+            // Creamos un nuevo arreglo dependiendo los parametros de la funcíon y las constantes de los años
             var newArray = data.filter(function (a){
                 return a.Country == country &&
                     a.Category == category && a.Year >= minVal && a.Year <= maxVal
             })
 
+            // Validamos que el nuevo arreglo tenga un valor
             if (newArray[0] === null || newArray[0] === undefined){
 
+                // En caso de no tener valores borramos todos los elementos rect y textos
                 const noData = rectElements
                     .selectAll('rect')
                     .remove()
    
-                const noLabels = drawChart
+                const noLabels = labelsLayer
                     .selectAll('text')
-                    .remove()                
+                    .remove()               
 
             } else {
 
+                // En caso de que el nuevo arreglo tenga datos
+
+                //Cambiamos todos los valores nulos a 0
                 Object.keys(newArray).map((key) => 
                     (newArray[key]['Inflation'] === null) ?  newArray[key]['Inflation'] = 0 :  newArray[key]['Inflation'])
     
+                // Definimos nuestros accesores
+
+                // El accesor 'x' contendra los años
                 const xAccessor = (d) => d.Year
+
+                // El accesor 'y' contendra los datos de inflación
                 const yAccessor = (d) => d.Inflation
     
+                // Definimos el dominio dinamico que cambiara dependiendo de los datos en el filtro de años (slider)
                 x.domain(d3.map(newArray, xAccessor))
     
+                // Definimos el dominio dinamico que cambiara dependiendo los filtros de categoria, país y años
                 y.domain([d3.min(newArray, yAccessor), d3.max(newArray, yAccessor)])
     
+                // Dibujo de elementos rect
                 const rect = rectElements
                     .selectAll('rect')
                     .data(newArray)
@@ -326,8 +359,11 @@ const load = async () => {
                     .attr('height', (d) => drawChart.style('height').slice(0,-2) - y(yAccessor(d)))
                     .attr('fill', 'rgb(75,172,198)')
 
+                // Dado que hacemos un cambio de nuestro accesor 'x' debemos cerrar el ciclo para reiniciar un nuevo dominimo y se ajusten
+                // los rect
                 rect.exit().remove()
     
+                // Creamos las etiquetas de datos para cada elemento rect
                 const labels = labelsLayer
                     .selectAll('text')
                     .data(newArray)
@@ -344,9 +380,11 @@ const load = async () => {
                     .attr('y', (d) => y(yAccessor(d))- (y(yAccessor(d))*0.01))
                     .text(yAccessor)
 
+                // Dado que hacemos un cambio de nuestro accesor 'x' debemos cerrar el ciclo para reiniciar un nuevo dominimo y se ajusten
+                // las etqiuetas
                 labels.exit().remove()
 
-                // Ejes
+                // Ejes dinamicos
                 const xAxis = d3.axisBottom(x)
                 const yAxis = d3.axisLeft(y)
 
@@ -364,35 +402,45 @@ const load = async () => {
         }
     }
 
-
+    // Función para obtener la inflación promedio de acuerdo a los parametros  y las constantes de los años
     const getAvgInflation = (country, category) => {
         
+        // Obtenemos los valores de los años minimo y maximo de nuestro slider
         const minVal = rangeInput.select('input.range-min').node().value,
         maxVal = rangeInput.select('input.range-max').node().value
 
+        // Validamos si nuestros parametros vienen vacíos
         if (country !== undefined && category !== undefined) {
 
+            // Creamos un nuevo arreglo dependiendo los parametros de la funcíon y las constantes de los años
             var newArray = data.filter(function (a){
                 return a.Country == country &&
                     a.Category == category &&
                     a.Year >= minVal && a.Year <= maxVal
             })
 
+            // Validamos que nuestro nuevo arreglo contenga datos
             if (newArray === undefined) {
 
+                // En caso de que no tenga datos mostramos un mensaje en la tarjeta de "No Data"
                 valueCard3
                     .text('No Data')
                     .style('color', 'black')
                 
             } else {
                 
+                // En caso de que nuestro nuevo arreglo tenga datos 
+                // Sumamos todos los valores del atributo 'Inflation'
                 const sumInflation = newArray.reduce((acc, value) => acc + value.Inflation, 0)
                 
+                // Filtramos los elementos que no esten vacios del nuevo arreglo y los guardamos en una constante
                 const totalRegistros = newArray.filter(element => element.Inflation != null)
                 
-        
+                // Calculamos el promedio dividiendo la suma de todos los valores del atributo 'Inflation' entre
+                // El total de registros (Quitamos los valores nulos ya que son considerados como 0 y estos no entran en el calculo de la suma)
                 const avgInflation = sumInflation / totalRegistros.length
         
+                // Validamos si el promedio es mayor a cero pintamos el texto de verde y en caso contrario pintamos el texto de rojo
                 if (avgInflation < 0){
                     valueCard3
                     .style('color', 'red')
@@ -400,43 +448,59 @@ const load = async () => {
                     valueCard3
                     .style('color', 'green')
                 }
+
+                // Asignamos el valor a la tarjeta redondeando a dos decimales
                 valueCard3.text(avgInflation.toFixed(2) + "%")
             }
         }   
 
     }
 
+    // Función para asignar la categoria a una tarjeta
     const getCategory = (category) => {
         valueCard2.text(category)
     }
 
+    // Función para obtener la variación dependiendo los parametros y el valor máximo del año seleccionado
     const getVarLastYear = (country, category) => {
 
+        // Obtenemos el año máximo del slider
         const maxVal = rangeInput.select('input.range-max').node().value
 
+        // Validamos si nuestos paramestros tienen información
         if (country !== undefined && category !== undefined){
 
+            // Creamos un nuevo arreglo que nos devuelva los resultados dependiendo los parametros y el año maximo y el año anterior
             var newArray = data.filter(function (a){
                 return a.Country == country &&
                     a.Category == category && a.Year >= (maxVal-1) && a.Year <= maxVal
             })
 
+            // Si nuestro nuevo arreglo no devuleve un dato mandamos el texto "No Data" a la tarjeta
             if (newArray[0] === undefined){
                 valueCard4
                     .text('No Data')
                     .style('color', 'black')
             } else {
 
+                // En caso de que nuestro nuevo arreglo tenga datos
+                // Obtendremos el último registro de nuestro array siendo el año máximo y lo dividimos entre el primer registro de nuestro
+                // Arreglo siendo el año anterior le restamos 1 para obtener la diferencia y multiplicamos por 100 para obtener el porcentaje
                 const varLastYear = (newArray[1]['Inflation'] / newArray[0]['Inflation'] - 1) * 100
 
+                // Validamos si nuestro resultado es distinto a un número u algún error como infinito
                 if (isNaN(varLastYear) || !isFinite(varLastYear)){
 
+                    // En caso de que nuestro resultado no sea un número o no sea finito regresamos a la tarjeta el texto 'No Data'
                     valueCard4
                         .text('No Data')
                         .style('color', 'black')
 
                 } else {
 
+                    // En caso de que nuestro resultado sea un número y sea finito
+                    // Si nuestro resultado es negativo pintamos el texto de rojo, caso contrario nuestro resultado es positivo
+                    // pintamos el texto de verde
                     if (varLastYear < 0){
                         valueCard4
                         .style('color', 'red')
@@ -444,6 +508,8 @@ const load = async () => {
                         valueCard4
                         .style('color', 'green')
                     }
+
+                    // Asignamos el valor a la tarjeta redondeando a dos decimales
                     valueCard4.text(varLastYear.toFixed(2) + "%")
                 }
 
@@ -561,8 +627,11 @@ const load = async () => {
         getVarLastYear(selectCountry.node().value, checksCategory.select('div.form-check input.form-check-input[name="Country"]:checked').node().value)
         drawChartRect(selectCountry.node().value, checksCategory.select('div.form-check input.form-check-input[name="Country"]:checked').node().value)
 
-})
+    })
 
+    function setDynamicXRange(){
+        x.range([0, drawChart.style('width').slice(0,-2)])
+    }
 }
 
 load()
